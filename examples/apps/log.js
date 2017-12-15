f49c011a42e10fe9b7b04e88495b06857e5ac986
Christian Harden <christian.harden@gmail.com>
2017-12-14 16:18:24 -0800

    (refactor) move prettier to devDependencies and update prettier

--- examples/apps/log.js
+++ examples/apps/log.js
@@ -12,8 +12,11 @@ GPT.on(Events.SLOT_RENDER_ENDED, event => {
 
     if (!event.isEmpty && event.size) {
         console.log(
-            `ad creative '${event.creativeId}' is rendered to slot '${divId}' of size '${event
-                .size[0]}x${event.size[1]}'`,
+            `ad creative '${
+                event.creativeId
+            }' is rendered to slot '${divId}' of size '${event.size[0]}x${
+                event.size[1]
+            }'`,
             event,
             targeting
         );
