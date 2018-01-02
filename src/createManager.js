41d14cd25a1d2dda1c4ca2e50fd0da24f00e14ee
Christian Shum-Harden <christian.harden@gmail.com>
2018-01-02 10:33:35 -0800

    (refactor) move prettier to devDependencies and update prettier (#67)

--- src/createManager.js
+++ src/createManager.js
@@ -190,12 +190,10 @@ export class AdManager extends EventEmitter {
             ].forEach(eventType => {
                 ["pubads", "content", "companionAds"].forEach(service => {
                     // there is no API to remove listeners.
-                    this.googletag
-                        [service]()
-                        .addEventListener(
-                            eventType,
-                            this._onEvent.bind(this, eventType)
-                        );
+                    this.googletag[service]().addEventListener(
+                        eventType,
+                        this._onEvent.bind(this, eventType)
+                    );
                 });
             });
             this._listening = true;
