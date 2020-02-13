aa0ee9ac3067cae3a93d6b05c3bcb6274e0f2abd
Alex Nosse <alex.nosse@meliuz.com.br>
2020-02-13 14:19:16 -0400

    Fixing webpack issues

--- webpack.config.js
+++ webpack.config.js
@@ -2,39 +2,46 @@ var webpack = require("webpack");
 var env = process.env.NODE_ENV;
 
 var reactExternal = {
-    root: "React",
-    commonjs2: "react",
-    commonjs: "react",
-    amd: "react"
+  root: "React",
+  commonjs2: "react",
+  commonjs: "react",
+  amd: "react"
 };
 
 var reactDomExternal = {
-    root: "ReactDOM",
-    commonjs2: "react-dom",
-    commonjs: "react-dom",
-    amd: "react-dom"
+  root: "ReactDOM",
+  commonjs2: "react-dom",
+  commonjs: "react-dom",
+  amd: "react-dom"
 };
 
 var config = {
-    externals: {
-        "react": reactExternal,
-        "react-dom": reactDomExternal
-    },
-    module: {
-        loaders: [
-            {test: /\.js$/, loaders: ["babel-loader"], exclude: /node_modules/}
-        ]
-    },
-    output: {
-        library: "ReactGPT",
-        libraryTarget: "umd"
-    },
-    plugins: [
-        new webpack.optimize.OccurenceOrderPlugin(),
-        new webpack.DefinePlugin({
-            "process.env.NODE_ENV": JSON.stringify(env)
-        })
-    ]
+  externals: {
+    "react": reactExternal,
+    "react-dom": reactDomExternal
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      enforce: "pre",
+      exclude: /node_modules/,
+      use: [{
+        loader: "babel-loader",
+        // more options in the optional jshint object
+
+      }],
+    }],
+  },
+  output: {
+    library: "ReactGPT",
+    libraryTarget: "umd"
+  },
+  plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.DefinePlugin({
+      "process.env.NODE_ENV": JSON.stringify(env)
+    })
+  ]
 };
 
 module.exports = config;
